87ce7f00768cee94c3a1499cfa538d924e81031a
thanglovemusic94 <thanglovemusic94@gmail.com>
2023-03-18 20:52:12 +0700

    Updates

---
+++ precache-manifest.2cfb29bf5b5a28685a8fa3a23344ec49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3f02a2277af0da5a75cca0b0511a0c",
+    "url": "/thanglovemusic94/index.html"
+  },
+  {
+    "revision": "6a8a20fe699085bce4a7",
+    "url": "/thanglovemusic94/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "e3a74a2d38f05dc9da7f",
+    "url": "/thanglovemusic94/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "6a8a20fe699085bce4a7",
+    "url": "/thanglovemusic94/static/js/2.0f07b985.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/thanglovemusic94/static/js/2.0f07b985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3a74a2d38f05dc9da7f",
+    "url": "/thanglovemusic94/static/js/main.dd33519e.chunk.js"
+  },
+  {
+    "revision": "bdf0b16484bc8a4010e5",
+    "url": "/thanglovemusic94/static/js/runtime-main.b925970c.js"
+  }
+]);
